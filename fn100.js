baca764e85ec314ff46316fa4d27a3a3a443534d
Marcin Kos <marcin.kos.krk@gmail.com>
2022-03-12 18:07:39 +0100

    fns summ array and max digit added

--- fn100.js
+++ fn100.js
@@ -98,3 +98,28 @@
 
 // const result = getWeather("https://danepubliczne.imgw.pl/api/data/synop/")
 // getWeatherData(result, 'temperatura')
+
+// Sum two arrays
+// const sumArrayItems = (arr1, arr2) => {
+//     const t = []
+
+//     for (let i=0; i < (arr1.lenght ? arr1.lenght : arr2.lenght); i++){
+//         t.push((arr1[i] || 0) + (arr2[i] || 0))
+//     }
+    
+//     return t
+// };
+
+// console.log(sumArrayItems([1, 2, 3], [4, 5]));  // [5, 7, 3]
+
+// const sumArrayItems = (arr1, arr2) => (arr1.length > arr2.length ? arr1 : arr2)
+//     .map((item, i) => (arr1[i] || 0) + (arr2[i] || 0))
+
+// console.log(sumArrayItems([1, 2, 3], [4, 5]));  // [5, 7, 3]
+
+
+// Return max from digits
+// const getMax = (digits) => `${digits}`.split('').sort((a, b) => b -a)[0]
+// const getMax2 = (digits) => Math.max(...`${digits}`.split('').map((item) => parseInt(item)))
+
+// console.log(getMax2(123456));    //6
